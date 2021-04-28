17bafd18e32aca68e9252d397348cc0816641790
eminokic <eminokic@iastate.edu>
2021-04-28 10:36:11 -0500

    Refactoring

--- src/main.c
+++ src/main.c
@@ -15,50 +15,6 @@
 
 void display_counts();
 
-int main(int argc, char **argv) {
-
-    //Command Input Verification
-    if (argc != 3) {
-        printf("Invalid number of arguments.\n   Usage: ./encrypt input-file output-file \n");
-        exit(-1); //exit with an error
-    }
-
-    printf("Enter the requested buffer size: ");
-    fflush(stdout);
-
-    char buff_size[256];
-    fgets(buff_size, 256, stdin);
-
-    init(argv[1], argv[2]);
-    while ((argc = read_input()) != EOF) {
-        count_input(argc);
-        argc = caesar_encrypt(argc);
-        count_output(argc);write_output(argc);
-    }
-    printf("End of file reached.\n");
-    display_counts();
-
-    return 1;
-
-}
-
-void display_counts() {
-    //print out the input file contents
-    for (int i = 0; i < 255; i++) {
-        if (((char) i) != '\n' && input_counts[i] > 0) {
-            printf("%c: %d\n", (char) i, input_counts[i]);
-        }
-    }
-
-    printf("Output File Contents: \n");
-    //print out the output file contents
-    for (int i = 0; i < 255; i++) {
-        if (((char) i) != '\n' && output_counts[i] > 0){
-            printf("%c: %d\n", (char) i, output_counts[i]);
-        }
-    }
-}
-
 /**
  * A node object to be implemented into the queue.
  */
@@ -73,7 +29,6 @@ struct node {
     node* past;
 
 };
-
 
 /**
    Implementation of a queue for the buffer
@@ -126,4 +81,58 @@ node *dequeue(priority_queue *queue) {
     }
     queue->current--;
     return n;
+}
+
+//Initializing input and output buffer variables
+priority_queue input_buffer;
+priority_queue output_buffer;
+
+/**
+ * Main Function for running encryption via console.
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char **argv) {
+
+    //Command Input Verification
+    if (argc != 3) {
+        printf("Invalid number of arguments.\n   Usage: ./encrypt input-file output-file \n");
+        exit(-1); //exit with an error
+    }
+
+    printf("Enter the requested buffer size: ");
+    fflush(stdout);
+
+    char buff_size[256];
+    fgets(buff_size, 256, stdin);
+
+    init(argv[1], argv[2]);
+    while ((argc = read_input()) != EOF) {
+        count_input(argc);
+        argc = caesar_encrypt(argc);
+        count_output(argc);write_output(argc);
+    }
+    printf("End of file reached.\n");
+    display_counts();
+
+    return 1;
+
+}
+
+void display_counts() {
+    //print out the input file contents
+    for (int i = 0; i < 255; i++) {
+        if (((char) i) != '\n' && input_counts[i] > 0) {
+            printf("%c: %d\n", (char) i, input_counts[i]);
+        }
+    }
+
+    printf("Output File Contents: \n");
+    //print out the output file contents
+    for (int i = 0; i < 255; i++) {
+        if (((char) i) != '\n' && output_counts[i] > 0){
+            printf("%c: %d\n", (char) i, output_counts[i]);
+        }
+    }
 }
